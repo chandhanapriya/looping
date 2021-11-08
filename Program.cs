dbe2dfc178d442e4cffe57a6a352690e84257aef
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 19:00:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace looping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Banking!");
+
+            // string AccountNo;
+            // AccountNo = Convert.ToString(12345678);
+            //var pie = 3.14;
+            Console.WriteLine("Enter your name \n");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Email \n");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter your mobileno \n");
+            var mobileno =Convert.ToInt32(Console.ReadLine());
+           // int mobileno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Accholder name is" + name + "Emailid is" + email + "Acholder mobileno" + mobileno + "Welcome to IBank");
+           
+
+
+
+
+
+        }
+    }
+}
